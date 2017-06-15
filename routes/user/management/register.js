9ad82a3488c977b2d9b5c0f941fac19949746a4e
DarkyZShadow <darkyzshadow@gmail.com>
2017-06-15 21:13:09 +0000

    [REVIEW] register.js

--- routes/user/management/register.js
+++ routes/user/management/register.js
@@ -1,51 +1,59 @@
-const config = require("../../../config/secret.js");
-const sql = require("mssql");
-const REGISTER_REQUEST = "SELECT TOP 1 Name FROM [dbo].[Account] WHERE [Name] = '";
-const INSERT_USER_REQUEST = "INSERT INTO dbo.Account (Authority, LastCompliment, LastSession, Name, Password, Email, RegistrationIp) VALUES (0, 0, 0, '";
+'use strict';
+const config = require('../../../config/secret.js');
+const sql = require('mssql');
 
-module.exports = function (req, res) {
-    let email = "" + req.body.email;
-    let username = "" + req.body.username;
-    let password = "" + req.body.password;
-    let passwordConfirmation = "" + req.body.passwordConfirmation;
-    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+const REGISTER_REQUEST = 'SELECT TOP 1 Name FROM [dbo].[Account] WHERE [Name] = \'';
+const INSERT_USER_REQUEST = 'INSERT INTO dbo.Account (Authority, LastCompliment, LastSession, Name, Password, Email, RegistrationIp) VALUES (0, 0, 0, \'';
 
-    if (validator.isEmail(email) && validator.isAlphanumeric(username) && validator.equals(password, passwordConfirmation) && (password.length >= 6 && password.length <= 15)) {
-        sql.connect(config.db).then(function () {
-            new sql.Request()
-                .query(`${REGISTER_REQUEST}${username}'`)
-                .then(function (recordset) {
-                    if (recordset.length === 0) {
-                        let hashedPassword = require('crypto').createHash('sha512').update(password).digest('hex');
-                        new sql.Request()
-                            .query(`${INSERT_USER_REQUEST}${username}"', '${hashedPassword}', '${email}', '${ip}')`)
-                            .then(function (recordset) {
-                                return res.render("register.ejs", {
-                                    success: global.translate.NICE_TRY_SQL_INJECTION
-                                });
-                            })
-                            .catch(function (err) {
-                                console.log(err);
-                                return res.render("register.ejs", {
-                                    error: global.translate.ERROR_IN_DATABASE
-                                });
-                            });
-                    } else {
-                        return res.render("register.ejs", {
-                            error: global.translate.USER_ALREADY_EXIST
-                        });
-                    }
-                })
-                .catch(function (err) {
-                    console.log(err);
-                    return res.render("register.ejs", {
-                        error: global.translate.ERROR_IN_DATABASE
-                    });
-                });
-        });
-    } else {
-        return res.render("register.ejs", {
-            error: "Données entrées invalides"
-        });
+async function register (req, res)
+{
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+    const passwordConfirmation = req.body.passwordConfirmation;
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+    /* Some checks */
+    if (!validator.isEmail(email))
+        return res.render('register.ejs', { error: 'Email incorrect' });
+    if (!validator.isAlphanumeric(username))
+        return res.render('register.ejs', { error: 'Nom d\'utilisateur incorrect' });
+    if (!validator.equals(password, passwordConfirmation))
+        return res.render('register.ejs', { error: 'Les mots de passe ne correspondent pas' });
+    if (password.length < 6 || password.length > 15)
+        return res.render('register.ejs', { error: 'Taille du mot de passe incorrecte' });
+
+    /* Await the BD connection & check if username is already taken */
+    let recordset;
+    try
+    {
+        await sql.connect(config.db);
+        recordset = await new sql.Request().query(`${REGISTER_REQUEST}${username}'`);
     }
-};
+    catch (error)
+    {
+        console.log(error);
+        return res.render('register.ejs', { error: global.translate.ERROR_IN_DATABASE });
+    }
+
+    /* If yes, throw an error */
+    if (recordset.length !== 0)
+        return res.render('register.ejs', { error: global.translate.USER_ALREADY_EXIST });
+
+    /* Register account */
+    let hashedPassword = require('crypto').createHash('sha512').update(password).digest('hex');
+    try
+    {
+        await new sql.Request().query(`${INSERT_USER_REQUEST}${username}"', '${hashedPassword}', '${email}', '${ip}')`);
+    }
+    catch (error)
+    {
+        console.log(error);
+        return res.render('register.ejs', { error: global.translate.ERROR_IN_DATABASE });
+    }
+
+    /* Done */
+    return res.render('register.ejs', { success: global.translate.REGISTER_SUCCESSFULL });
+}
+
+module.exports = register;
