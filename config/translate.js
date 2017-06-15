a6ab8a960f1fbfe1d0e2d2d33f820236e35fc339
Unknown <blowaxd693@gmail.com>
2017-06-15 18:13:29 +0200

    [FIX] translate a bit

--- config/translate.js
+++ config/translate.js
@@ -1,5 +1,6 @@
 module.exports = {
     "NICE_TRY_SQL_INJECTION": "Haha, t'as essayé, mais t'es nul désolé",
     "USER_ALREADY_EXIST": "",
-    "ERROR_IN_DATABASE": ""
+    "ERROR_IN_DATABASE": "",
+    "REGISTER_SUCCESSFULL": ""
 };
