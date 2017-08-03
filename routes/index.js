5adc89e59351cae33015f8a19cea8ed0ab55b112
DarkyZShadow <s.soulie@hotmail.fr>
2017-08-03 21:01:10 +0200

    Remove package-lock.json

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 /**
  * Created by Blowa on 6/15/2017.
  */
+const launcher = require("./launcher/index");
+const user = require("./user/index");
 
-const launcher = require("./launcher/index.js");
-const user = require("./user/index.js");
-
-module.exports = {};
-exports.launcher = require("./launcher/index.js");
-exports.user = require("./user/index.js");
+module.exports = {
+    launcher: require("./launcher/index"),
+    user: require("./user/index")
+};
