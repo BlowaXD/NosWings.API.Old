4a0b541fe62bde343e0ad0db9180f30f19a2ce16
DarkyZ <s.soulie@hotmail.fr>
2017-09-10 22:37:52 +0200

    [FINISH] Nosmall is now working well

--- routes/shop/buy.js
+++ routes/shop/buy.js
@@ -1,43 +1,121 @@
 'use strict';
 const sql = require('mssql');
-const GET_ACCOUNT = `
-    SELECT TOP 1 [Permissions], [Money]
+
+const INSERT_LOG = `
+    INSERT INTO _GF_CS_BuyLogs (AccountId, Type, [Value])
+    VALUES (
+        (SELECT AccountId FROM Account WHERE Name = @username AND [Password] = @password),
+        1,
+        @price
+    )`;
+const UPDATE_MONEY = `
+    UPDATE b
+        SET b.Money = b.Money - @price
+    FROM _GF_CS_Accounts AS b
+    JOIN Account AS a
+        ON a.[AccountId] = b.[AccountId]
+    WHERE a.[Name] = @username
+        AND [Password] = @password`;
+const GET_MONEY = `
+    SELECT TOP 1 [Money]
     FROM [dbo].[Account]
     LEFT JOIN _GF_CS_Accounts
         ON [_GF_CS_Accounts].[AccountId] = [Account].[AccountId]
     WHERE [Name] = @username
         AND [Password] = @password`;
-const GET_PACK_BY_ID = `Jekonépalakouéri désolé`;
+const GET_PACK = `
+    SELECT *
+    FROM _GF_CS_Packs AS p
+    JOIN _GF_CS_PackItem AS i
+        ON p.PackId = i.PackId
+    WHERE p.PackId = @packid`;
+const GET_CHARACTER = `
+    SELECT TOP 1 CharacterId
+    FROM Account AS a
+    JOIN [Character] AS c
+        ON a.AccountId = c.AccountId
+    WHERE a.[Name] = @username
+        AND a.[Password] = @password
+        AND c.State = 1
+        AND c.Name = @character`;
+const SEND_MAIL = `
+    INSERT INTO [Mail] (
+        [AttachmentAmount],
+        [AttachmentRarity],
+        [AttachmentUpgrade],
+        [AttachmentVNum],
+        [IsOpened],
+        [IsSenderCopy],
+        [ReceiverId],
+        [SenderClass],
+        [SenderGender],
+        [SenderHairColor],
+        [SenderHairStyle],
+        [SenderId],
+        [SenderMorphId],
+        [Title])
+    VALUES (
+        @AttachementAmount,
+        @AttachementRarity,
+        @AttachmentUpgrade,
+        @AttachmentVNum,
+        '0',
+        '0',
+        @character_id,
+        '0',
+        '0',
+        '0',
+        '0',
+        '13',
+        '-1',
+        @Title)`;
 
 async function get(req, res)
 {
     const server = global.config.servers[req.user.server];
     const username = req.user.username;
     const hashedPassword = req.user.hashedPassword;
+    const character = req.body.character;
+    const packId = parseInt(req.body.PackId);
 
     /* Some checks */
     if (!server)
         return res.status(403).send({ success: false, error: global.translate.WRONG_SERVER });
-    if (!username)
-        return res.status(403).send({ success: false, error: global.translate.WRONG_USERNAME });
-    if (!hashedPassword)
-        return res.status(403).send({ success: false, error: global.translate.WRONG_PASSWORD });
+    if (!username || !hashedPassword || !packId || !character )
+        return res.status(400).send({ success: false, error: global.translate.BAD_QUERY });
 
     /* Get account */
-    let account;
-    let pack;
+    let money;
+    let items;
+    let character_id;
     try
     {
-        const request = await server.db.request()
+        /* Check character name */
+        let request = await server.db.request()
             .input('username', sql.VarChar, username)
             .input('password', sql.VarChar, hashedPassword)
-            .query(GET_ACCOUNT);
-        account = request.recordset || [];
+            .input('character', sql.VarChar, character)
+            .query(GET_CHARACTER);
+        character_id = request.recordset[0].CharacterId
+        if (!character_id)
+            throw new Error(`Unable to find character ${character}`);
 
-        const request_pack = await server.db.request()
-            .input('packid', sql.Int, packid)
-            .query(GET_PACK_BY_ID);
-        pack = request.recordset || [];
+        /* Get money */
+        request = await server.db.request()
+            .input('username', sql.VarChar, username)
+            .input('password', sql.VarChar, hashedPassword)
+            .query(GET_MONEY);
+        money = parseInt(request.recordset[0].Money);
+        if (!Number.isInteger(money))
+            throw new Error(`Unable to find ${username}'s' money`);
+
+        /* Get pack's items */
+        request = await server.db.request()
+            .input('packid', sql.Int, packId)
+            .query(GET_PACK);
+        items = request.recordset;
+        if (!items || items.length < 1)
+            throw new Error(`Unable to find items for pack with ID ${packId}`);
     }
     catch (error)
     {
@@ -45,21 +123,83 @@ async function get(req, res)
         return res.status(500).send({ success: false, error: global.translate.ERROR_IN_DATABASE });
     }
 
-    if (account.Money < pack.Price)
-    {
+    if (money < items[0].Price)
         return res.status(403).send({success: false, error: global.translate.NOT_ENOUGH_MONEY});
-    }
 
-    if (pack.Type === 1)
+    /* Create item list */
+    let item_list;
+    if (items[0].Type === 1)
     {
-        // DO RANDOM PICK A PACKITEM ID
+        const tmp = items[Math.floor(Math.random() * items.length)];
+        item_list = [{
+            AttachementAmount: tmp.ItemQuantity,
+            AttachementRarity: tmp.ItemDesign,
+            AttachmentUpgrade: tmp.ItemUpgrade,
+            AttachmentVNum: tmp.ItemVnum,
+            Title: req.user.server
+        }];
     }
     else
     {
-        // SEND EVERY PACK ITEM ID
+        item_list = items.map(i => new Object({
+            AttachementAmount: i.ItemQuantity,
+            AttachementRarity: i.ItemDesign,
+            AttachmentUpgrade: i.ItemUpgrade,
+            AttachmentVNum: i.ItemVnum,
+            Title: req.user.server
+        }));
     }
 
-    /* INSERT THE MAIL AND REMOVE ACCOUNT MONEY */
+    /* Remove golds */
+    try
+    {
+        await server.db.request()
+            .input('price', sql.Int, items[0].Price)
+            .input('username', sql.VarChar, username)
+            .input('password', sql.VarChar, hashedPassword)
+            .query(UPDATE_MONEY);
+    }
+    catch (error)
+    {
+        console.log(error);
+        return res.status(500).send({ success: false, error: global.translate.ERROR_IN_DATABASE });
+    }
+
+    /* Send items */
+    for (const i of item_list)
+    {
+        try
+        {
+            await server.db.request()
+                .input('character_id', sql.Int, character_id)
+                .input('AttachementAmount', sql.Int, i.AttachementAmount)
+                .input('AttachementRarity', sql.Int, i.AttachementRarity)
+                .input('AttachmentUpgrade', sql.Int, i.AttachmentUpgrade)
+                .input('AttachmentVNum', sql.Int, i.AttachmentVNum)
+                .input('Title', sql.VarChar, i.Title)
+                .query(SEND_MAIL);
+        }
+        catch (error)
+        {
+            console.log(error);
+            return res.status(500).send({ success: false, error: global.translate.ERROR_IN_DATABASE });
+        }
+    }
+
+    /* Log */
+    try
+    {
+        await server.db.request()
+            .input('price', sql.Int, items[0].Price)
+            .input('username', sql.VarChar, username)
+            .input('password', sql.VarChar, hashedPassword)
+            .query(INSERT_LOG);
+    }
+    catch (error)
+    {
+        console.log(error);
+        return res.status(500).send({ success: false, error: global.translate.ERROR_IN_DATABASE });
+    }
 
     res.sendStatus(200);
 }
