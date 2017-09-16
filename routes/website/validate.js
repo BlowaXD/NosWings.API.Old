a3d8e9c81d0fe3c1b28a070c4b9b2c275e93ba85
Blowa <blowaxd693@gmail.com>
2017-09-16 16:42:03 +0200

    [FINISH] Validation

--- routes/website/validate.js
+++ routes/website/validate.js
@@ -9,26 +9,23 @@ async function validate(req, res) {
     let validationToken = req.params.validationtoken;
 
     if (!validator.isAlphanumeric(validationToken))
-        return;
+        return res.status(403).send({success: false, error: global.translate.WRONG_USERNAME});
     let recordset;
     try {
-        await sql.connect(config.db);
-
         let request = new sql.Request();
         request.input('verifToken', sql.VarChar, validationToken);
         recordset = await request.query(`${REQUEST_CHECK_ACCOUNT}`);
     }
     catch (error) {
-        console.log(error);
-        return res.status(500).send({error: global.translate.ERROR_IN_DATABASE});
+        return res.status(500).send({success: false, error: global.translate.ERROR_IN_DATABASE});
     }
     recordset = recordset.recordset;
 
     if (recordset.length === 0)
-        return res.status(500).send({error: global.translate.ERROR_IN_DATABASE});
+        return res.status(500).send({success: false, error: global.translate.ERROR_IN_DATABASE});
 
     if (recordset[0].Authority !== -1)
-        return res.status(500).send({error: global.translate.ACCOUNT_ALREADY_VALIDATED});
+        return res.status(403).send({error: global.translate.ACCOUNT_VALIDATION_ALREADY});
 
     try {
         const request = new sql.Request();
@@ -36,8 +33,6 @@ async function validate(req, res) {
         await request.query(`${REQUEST_VALIDATE_ACCOUNT}`);
     }
     catch (error) {
-        sql.close();
-        console.log(error);
         return res.status(500).send({error: global.translate.ERROR_IN_DATABASE});
     }
     sql.close();
