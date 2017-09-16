bd4ab08277865f84734d4181bea6a6357d47f98c
Blowa <blowaxd693@gmail.com>
2017-09-16 16:51:12 +0200

    [FIX] Register again

--- routes/website/register.js
+++ routes/website/register.js
@@ -7,22 +7,13 @@ const fs = require('fs');
 const REGISTER_REQUEST = 'SELECT TOP 1 Name FROM [dbo].[Account] WHERE [Name] = @username;';
 const INSERT_USER_REQUEST = 'INSERT INTO dbo.Account (Authority, Name, Password, Email, RegistrationIp, VerificationToken) VALUES (-1, @username, @password, @email, @registrationIp, @veriftoken);';
 
-String.prototype.replaceAll = function(search, replacement) {
+String.prototype.replaceAll = function (search, replacement) {
     const target = this;
     return target.split(search).join(replacement);
 };
 
 async function register(req, res) {
-    try {
-        await recaptcha.validateRequest(req);
-    }
-    catch (error)
-    {
-        return res.render('register', {
-            error: "Recaptcha fail"
-        });
-    }
-
+    const server = global.config.servers[req.body.server];
     const account = {
         email: req.body.email,
         username: req.body.username,
@@ -31,63 +22,63 @@ async function register(req, res) {
         ip: req.body.ip,
     };
 
-    if (!account.email || !account.username || !account.password || !account.ip)
-    {
+    if (!account.email || !account.username || !account.password || !account.ip) {
         return res.status(403).send({error: global.translate.WRONG_USERNAME});
     }
 
     /* Some checks */
     if (!validator.isEmail(account.email))
-        return res.status(403).send({error: global.translate.WRONG_USERNAME});
+        return res.status(200).send({success: false, error: global.translate.WRONG_USERNAME});
     if (!validator.isAlphanumeric(account.username))
-        return res.status(403).send({error: global.translate.WRONG_USERNAME});
+        return res.status(200).send({success: false, error: global.translate.WRONG_USERNAME});
     if (!validator.equals(account.password, account.passwordConfirmation))
-        return res.status(403).send({error: global.translate.WRONG_USERNAME});
+        return res.status(200).send({success: false, error: global.translate.WRONG_USERNAME});
     if (account.password.length < 6 || account.password.length > 25)
-        return res.status(403).send({error: global.translate.WRONG_USERNAME});
+        return res.status(200).send({success: false, error: global.translate.WRONG_USERNAME});
 
     /* Await the BD connection & check if username is already taken */
     let recordset;
     try {
-        await sql.connect(config.db);
-
-        const request = new sql.Request();
-        request.input('username', sql.VarChar, account.username);
-        recordset = await request.query(`${REGISTER_REQUEST}`);
+        const request = await server.db.request()
+            .input('username', sql.VarChar, account.username)
+            .query(REGISTER_REQUEST);
+        recordset = request.recordset || [];
     }
     catch (error) {
-        return res.status(500).send({error: global.translate.ERROR_IN_DATABASE});
+        return res.status(200).send({success: false, error: global.translate.ERROR_IN_DATABASE});
     }
 
     recordset = recordset.recordset;
     /* If yes, throw an error */
-    if (recordset.length !== 0)
-        return res.status(403).send({error: global.translate.USER_ALREADY_EXIST});
+    if (recordset.length !== 0) {
+        return res.status(200).send({success: false, error: global.translate.USER_ALREADY_EXIST});
+    }
 
     /* Register account */
     let hashedPassword = require('crypto').createHash('sha512').update(account.password).digest('hex');
     let verificationToken = crypto.randomBytes(16).toString('hex');
     try {
-        const request = new sql.Request();
+        const request = await server.db.request()
+            .input('username', sql.VarChar, account.username)
+            .input('password', sql.VarChar, account.hashedPassword)
+            .input('email', sql.VarChar, account.email)
+            .input('registrationIp', sql.VarChar, account.ip)
+            .input('veriftoken', sql.VarChar, verificationToken)
+            .query(INSERT_USER_REQUEST);
 
-        request.input('username', sql.VarChar, account.username);
-        request.input('password', sql.VarChar, account.hashedPassword);
-        request.input('email', sql.VarChar, account.email);
-        request.input('registrationIp', sql.VarChar, account.ip);
-        request.input('veriftoken', sql.VarChar, verificationToken);
-        await request.query(`${INSERT_USER_REQUEST}`);
+        recordset = request.recordset || [];
     }
     catch (error) {
-        return res.status(500).send({error: global.translate.ERROR_IN_DATABASE});
+        return res.status(200).send({success: false, error: global.translate.ERROR_IN_DATABASE});
     }
 
     /* SEND MAIL TO CONFIRM */
     let transporter = nodemailer.createTransport(config.smtp);
     let mailOptions = {
         from: global.translate.REGISTRATION_EMAIL_SENDER + '<' + config.smtp.auth.user + '>', // sender address
-        to: email, // list of receivers
+        to: account.email, // list of receivers
         subject: global.translate.REGISTRATION_EMAIL_SUBJECT, // Subject line
-        html: ''+fs.readFileSync("./mails/mail.html", 'utf8')
+        html: '' + fs.readFileSync("./mails/mail.html", 'utf8')
     };
 
     mailOptions.html = mailOptions.html.replaceAll("{LOGO}", config.urls.logo);
@@ -97,21 +88,16 @@ async function register(req, res) {
     mailOptions.html = mailOptions.html.replaceAll("{EMAIL}", account.email);
     mailOptions.html = mailOptions.html.replaceAll("{MESSAGE}", global.translate.REGISTRATION_MESSAGE);
     mailOptions.html = mailOptions.html.replaceAll("{BUTTON_DESCRIPTION}", global.translate.REGISTRATION_BUTTON_DESCRIPTION);
-    mailOptions.html = mailOptions.html.replaceAll("{BUTTON_TITLE}", global.translate.REGISTRATION_BUTTON_TITLE);
-    mailOptions.html = mailOptions.html.replaceAll("{BUTTON_LINK}", config.urls.validate + verificationToken);
-    mailOptions.html = mailOptions.html.replaceAll("{FOOTER_DESCRIPTION}", global.translate.REGISTRATION_FOOTER_DESCRIPTION);
-    mailOptions.html = mailOptions.html.replaceAll("{FOOTER_STAFF_NAME}", global.translate.STAFF_NAME);
-    mailOptions.html = mailOptions.html.replaceAll("{FORUM_LINK}", config.urls.forum);
-    mailOptions.html = mailOptions.html.replaceAll("{DISCORD_LINK}", config.urls.discord);
-    mailOptions.html = mailOptions.html.replaceAll("{SITE_LINK}", config.urls.site);
+    mailOptions.html = mailOptions.html.replaceAll("{BUTTON_TITLE}", "Valider mon compte");
+    mailOptions.html = mailOptions.html.replaceAll("{BUTTON_LINK}", "https://noswings.fr/register/validate/" + verificationToken);
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.error(error);
-            return res.status(500).send({error: global.translate.REGISTRATION_EMAIL_ERROR});
+            return res.status(500).send({success: false, error: global.translate.REGISTRATION_ERROR_MAIL});
         }
         /* REGISTRATION DONE SUCCESSFULLY */
-        return res.status(200).send({success: global.translate.REGISTER_SUCCESSFULL});
+        return res.status(200).send({success: true, data: global.translate.REGISTRATION_SUCCESS});
     });
 }
 
