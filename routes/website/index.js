6822f2227724460cfb1729dcdd4ff26ea56ee67a
Blowa <blowaxd693@gmail.com>
2017-09-21 00:10:49 +0200

    [FIX] Mails on register

--- routes/website/index.js
+++ routes/website/index.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 
 router.get('/get_news', require('./home.js'));
-router.get('/register/:validationtoken', require('./validate.js'));
+router.get('/register/validate/:validationtoken', require('./validate.js'));
 router.post('/register', require('./register.js'));
 
 module.exports = router;
