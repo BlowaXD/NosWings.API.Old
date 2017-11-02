040bc578648d6ab9d8bdbeeecfcf6d2cc56215b5
Blowa <blowaxd693@gmail.com>
2017-11-02 23:37:35 +0100

    Fix queries

--- routes/admin/addMoney.js
+++ routes/admin/addMoney.js
@@ -11,7 +11,7 @@
 const sql = require('mssql');
 const ADD_MONEY_QUERY = 'UPDATE a SET Money = Money + @addMoney FROM _GF_CS_Accounts AS a JOIN [Character] AS c ON c.AccountId = a.AccountId WHERE c.Name = @characterName;';
 const ADD_ACCOUNT = 'INSERT INTO [opennos].[dbo].[_GF_CS_Accounts] ([AccountId], [Money], [Permissions]) VALUES (@AccountId, 0, 0);';
-const GET_ACCOUNT = 'SELECT AccountId FROM [_GF_CS_Accounts] AS acc JOIN [Character] AS character ON character.AccountId = a.AccountId WHERE character.Name = @characterName;';
+const GET_ACCOUNT = 'SELECT c.AccountId FROM [_GF_CS_Accounts] AS acc JOIN [Character] AS c ON c.AccountId = acc.AccountId WHERE c.Name = @characterName;';
 const GET_ACCOUNTID = 'SELECT AccountId FROM [Character] WHERE Name = @characterName;';
 
 async function post(req, res) {
@@ -26,7 +26,7 @@ async function post(req, res) {
     }
 
     let recordset;
-
+    let accountId;
     try {
         const request = await server.db.request()
             .input('characterName', sql.VarChar, account.Character)
@@ -39,20 +39,21 @@ async function post(req, res) {
     }
 
     if (recordset.length === 0) {
-        let accountId;
         try {
             const request = await server.db.request()
                 .input('characterName', sql.VarChar, account.Character)
                 .query(`${GET_ACCOUNTID}`);
-            accountId = request.recordset || [];
+            console.log(request);
+            accountId = request.recordset[0].AccountId || [];
         }
         catch (error) {
             console.error(error);
             return res.status(500).send({success: false, error: global.translate.ERROR_IN_DATABASE});
         }
+
         try {
             await server.db.request()
-                .input('AccountId', sql.Int, accountId)
+                .input('AccountId', sql.BigInt, accountId)
                 .query(`${ADD_ACCOUNT}`);
         }
         catch (error) {
@@ -66,7 +67,6 @@ async function post(req, res) {
             .input('addMoney', sql.Int, account.Money)
             .input('characterName', sql.VarChar, account.Character)
             .query(`${ADD_MONEY_QUERY}`);
-        recordset = request.recordset || [];
     }
     catch (error) {
         console.log(error);
